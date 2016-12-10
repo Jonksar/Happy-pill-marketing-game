0b9d60737a7c4bc63bd364d37b2739adc8c9ef17
Bob Kelso <bob@example.com>
2016-12-11 00:53:59 +0200

    Tweak Demothan (initial height, fixedY -> originY)

--- Scripts/DemontanHovering.cs
+++ Scripts/DemontanHovering.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DemontanHovering : MonoBehaviour {
-	private float fixedY;
+	private float originY;
 	private float hoverOffset;
 
 	void Start () {
-		fixedY = transform.position.y;
+		originY = transform.position.y;
 		hoverOffset = Random.value;
 	}
 
 	void Update () {
 		Vector3 pos = transform.position;
 		long milliseconds = System.DateTime.Now.Ticks / 100000 % 31416;
-		transform.position = new Vector3 (pos.x, fixedY + Mathf.Sin (hoverOffset + milliseconds / 30.0f) * 0.4f, pos.z);
+		transform.position = new Vector3 (pos.x, originY + Mathf.Sin (hoverOffset + milliseconds / 30.0f) * 0.4f, pos.z);
 	}
 }
