65e8fc260204bfd8ee4043e8d03b10fe50f8f0c9
Bob Kelso <bob@example.com>
2016-12-10 12:05:31 +0200

    Fix bad merge, bug

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -6,13 +6,11 @@ public class PlayerController : MonoBehaviour {
 	private SpriteRenderer sprite;
 	public float speed = 1.0f;
 
-    public float speed 
-
 	// Use this for initialization
 	void Start () {
 		sprite = this.GetComponent<SpriteRenderer>();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		Vector3 pos = transform.position;
