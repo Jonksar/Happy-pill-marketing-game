ea3e981fc1b1a0701966d6fed06f9c72feaf4341
Bob Kelso <bob@example.com>
2016-12-10 13:50:23 +0200

    Add spawner

---
+++ Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+	public GameObject obj;
+	public Direction direction;
+	public float spawnRateSeconds;
+
+	void Start () {
+		InvokeRepeating("Spawn", spawnRateSeconds, spawnRateSeconds);
+	}
+
+	void Spawn() {
+		//Enemy enemy = (Enemy)Instantiate(obj);
+		//enemy.direction = direction;
+		GameObject o = Instantiate(obj, transform.position, transform.rotation);
+		Enemy test = o.GetComponent<Enemy>();
+		//test.transform.position = transform;
+		test.direction = direction;
+	}
+}
