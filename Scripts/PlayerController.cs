c17f959f3d3b55df21800cac279de316b2dfc3de
Bob Kelso <bob@example.com>
2016-12-10 12:03:07 +0200

    Basic player and movement, placeholder

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+	private SpriteRenderer sprite;
+	public float speed = 1.0f;
 
     public float speed 
 
 	// Use this for initialization
 	void Start () {
-		
+		sprite = this.GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Vector3 pos = transform.position;
+		float inc = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+
+		sprite.flipX = inc > 0;
+		transform.position += new Vector3(inc, 0, 0);
 	}
 }
