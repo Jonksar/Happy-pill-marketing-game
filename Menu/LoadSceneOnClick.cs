fb971b0053595a6427d7ca244eb9bc1c2c294d1a
Joonatan Samuel <joonatan.samuel@gmail.com>
2016-12-10 17:37:15 +0200

    menu stuff?

---
+++ Menu/LoadSceneOnClick.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour {
+
+	public void LoadByIndex(int index) {
+		SceneManager.LoadScene (index);
+	}
+
+}
