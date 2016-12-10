4a8fa5e3360ae09a25ed013263185c5b2326b9b6
Bob Kelso <bob@example.com>
2016-12-10 13:29:57 +0200

    Add placeholder enemy

---
+++ Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction {
+	left,
+	right
+};
+
+// Enemy base class for common behaviour
+public class Enemy : MonoBehaviour {
+	public float speed;
+	public int health;
+	public Direction direction;
+
+	void Start () {
+		// ...
+	}
+
+	void Update () {
+		float slop = 1.0f;
+		float inc = Time.deltaTime * speed * (direction == Direction.left ? -1 : 1);
+		transform.position += new Vector3(inc, 0, 0);
+
+		if (transform.position.x > -slop && transform.position.x < slop) {
+			Destroy(gameObject);
+		}
+	}
+}
