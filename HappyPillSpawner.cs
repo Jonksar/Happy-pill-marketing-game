3fa41ff2cae0d95313959cc598fa71d6e71add78
gamithra <gamittraaah@gmail.com>
2016-12-12 16:43:45 +0200

    happy pill bug fixed

--- HappyPillSpawner.cs
+++ HappyPillSpawner.cs
@@ -7,8 +7,8 @@ public class HappyPillSpawner : MonoBehaviour {
 	public bool pillSpawned = false;
 	// Use this for initialization
 	void Start () {
-		Invoke ("SpawnPill", 3f);
-		InvokeRepeating ("SpawnPill", 30f, 20f);
+		Invoke ("SpawnPill", 4f);
+		//InvokeRepeating ("SpawnPill", 30f, 20f);
 	}
 	
 	// Update is called once per frame
@@ -17,6 +17,7 @@ public class HappyPillSpawner : MonoBehaviour {
 			GameObject.Find ("Environment").GetComponent<Environment> ().HappyPillStartUseEat();
 			GameObject.Find ("Happy Pill").GetComponent<Image> ().color = Color.clear;
 			GameObject.Find ("Text").GetComponent<Text> ().color = Color.clear;
+			pillSpawned = false;
 		}
 	}
 
@@ -26,5 +27,7 @@ public class HappyPillSpawner : MonoBehaviour {
 			GameObject.Find ("Happy Pill").GetComponent<Image> ().color = Color.white;
 			GameObject.Find ("Text").GetComponent<Text> ().color = Color.black;
 		}
+		Invoke ("SpawnPill", 23f);
+
 	}
 }
